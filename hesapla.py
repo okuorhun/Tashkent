03f946c96ebe05a8d52eba8d3b05c87ce657109d
okuorhun <oku.orhun@gmail.com>
2022-04-28 10:26:06 +0300

    hesapla 1-nusqa

---
+++ hesapla.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 09:03:39 2022
+
+@author: murat
+"""
+
+'''
+import functions
+import selam
+
+t= functions.sum(10, 20)
+print(t)
+
+selam.merhaba("Ali")
+
+
+
+from functions import power
+print(power(2,4))
+
+'''
+from selam import merhaba,sub
+
+merhaba("Tömür")
+print(sub(2,3))
+
+from functions import *
+
+print(average(4, 7))
+print(power(4,3))
+
+def power(a,b):
+	return a*b
+
+print(power(2,3))
+
+from functions import power as mypower
+
+print(mypower(2, 3))
+
+import functions
+
+print(functions.power(2, 3))
+
+from myclass import MyClass
+
+t=MyClass()
+
+k= [1,2,3,4,5,6,7,8]
+t.printList(k)
+
+
+
+
+
+
+
+
+
